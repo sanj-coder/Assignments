5aa3eb6e95913016eb4d8f8f216dc4b73b83e9ac
sanj-coder <sanjaybujiii2906@gmail.com>
2024-08-28 11:38:44 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,31 @@
+package javaPratice;
+
+public class Mobile {
+           public static void main (String []args) {
+  
+        	   
+        	          String mobile="Vivo";
+        	          String model="vivo v11 pro";
+        	          int price=11000;
+        	          char logo='V';
+        	          float discount=50.88f;
+        	          double SARValue=4.443544534d;
+        	          long IMEInumber=445429007543698654l;
+        	          boolean expensive=true;
+        	          System.out.print("Mobile:"+mobile);
+        	          System.out.print("Model:"+model);
+        	          System.out.print("Price:"+price);
+        	          System.out.print("Logo:"+logo);
+        	          System.out.print("Discount"+discount);
+        	          System.out.print("SARValue:"+SARValue);
+        	          System.out.println("IMEInumber:"+IMEInumber);
+        	          System.out.println("Expensive:"+expensive);
+        	  	   	  System.out.println("Mobile:"+mobile+" "+"Model:"+model+" "+"Price:"+price+" "+"Logo:"+logo+" "+"Discount"+discount+" " +"SARValue:"+" " +"IMEInumber:"+IMEInumber+" "+"Expensive:"+expensive);
+           }
+}
+
+        	          
+        	        		  
+        	        		  
+        	         
+        	        		  
